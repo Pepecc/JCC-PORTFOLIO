991669a2d2776bf31b241bb894be577f21cc7253
Pepe <pepecc83@gmail.com>
2026-01-03 21:48:04 +0100

    Añadido sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jcc-dev.onrender.com/',
+
   server: {
     host: true,
-    port: 4321
-  }
+    port: 4321,
+  },
+
+  integrations: [sitemap()],
 });
